70a97b8b1f7d4cd20b3b5bd001850aed2f074f31
Christian Sanches <christian.sanches@nubank.com.br>
2023-11-20 13:06:22 -0300

    changed message

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
     return (
         <div>
             <h1>
-                Oi Amor! Estou aqui na piscina te esperando! {`<3`}
+                Oi Amor! Estou aqui na piscina te esperando! {`<3`} Vem logo! 
             </h1>
         </div>
     )
