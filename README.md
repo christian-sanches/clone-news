65e548aca35fd8914c281e767693551604204624
Christian Sanches <christian@endless-point.org>
2024-02-25 17:17:14 -0300

    prettier fix all project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-news
+
 A study project of the tabnews.com.br. Learning is never outdated!
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Home: React.FC = () => {
-    return (
-        <div>
-            <h1>
-                Oi Amor! Estou aqui na piscina te esperando! {`<3`}
-            </h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Oi Amor! Estou aqui na piscina te esperando! {`<3`}</h1>
+    </div>
+  );
+};
 
 export default Home;
