865a37117549eb6156cc94ca5610066430334694
Christian Sanches <christian@endless-point.org>
2024-05-26 14:43:57 -0300

    add updated at to get status

--- app/api/v1/status/route.ts
+++ app/api/v1/status/route.ts
@@ -1,10 +1,9 @@
-import db from "infra/database";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const result = await db.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
   return NextResponse.json({
-    message: "Alunos do curso.dev são pessoas acima da média",
+    updated_at: updatedAt,
   });
 }
