4dccb0f8abb441a938763d5713e36d13c20d51fa
Christian Sanches <christian@endless-point.org>
2024-05-26 14:13:51 -0300

    env development, infra, tests

--- app/api/v1/status/route.ts
+++ app/api/v1/status/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
+import db from "../../../../infra/database";
 
 export async function GET() {
+  const result = await db.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   return NextResponse.json({
     message: "Alunos do curso.dev são pessoas acima da média",
   });
